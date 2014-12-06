671cba3e7257ab2b597a5525defe4189f3d10c29
mahfuzrahman <mahfuz@nascenia.com>
2014-12-06 20:53:58 +0600

    deleted 3 apk files (what is generated by 'cordova build android ' command)

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/nascenia/projects/WTSS-APP/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/nascenia/projects/WTSS-APP/platforms/android/ant-build/CordovaApp.ap_ \
-/home/nascenia/projects/WTSS-APP/platforms/android/ant-build/classes.dex \
-/home/nascenia/projects/WTSS-APP/platforms/android/CordovaLib/ant-build/classes.jar \
